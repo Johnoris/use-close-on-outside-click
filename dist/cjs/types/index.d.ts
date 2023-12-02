3bd37d4df522b2dc3bf1d647c54c1bca20ac475f
John <johnorisanwo@gmail.com>
2023-12-02 18:13:25 +0100

    ignoring files

--- dist/cjs/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, RefObject, SetStateAction } from "react";
-interface useCloseOnClickOutsideReturnType extends Array<boolean | Dispatch<SetStateAction<boolean>>> {
-    0: boolean;
-    1: Dispatch<SetStateAction<boolean>>;
-}
-declare const useCloseOnClickOutside: (ref: RefObject<any>) => useCloseOnClickOutsideReturnType;
-export default useCloseOnClickOutside;
--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { RefObject, Dispatch, SetStateAction } from 'react';
-
-interface useCloseOnClickOutsideReturnType extends Array<boolean | Dispatch<SetStateAction<boolean>>> {
-    0: boolean;
-    1: Dispatch<SetStateAction<boolean>>;
-}
-declare const useCloseOnClickOutside: (ref: RefObject<any>) => useCloseOnClickOutsideReturnType;
-
-export { useCloseOnClickOutside as default };
